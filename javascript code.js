8f498fdc0dc29c731a5c9328f0ba34a39a87ff16
Vijai.S <156160518+Vijai1274@users.noreply.github.com>
2024-03-09 01:03:10 +0530

    Javascript code.js

---
+++ javascript code.js
@@ -0,0 +1,8 @@
+const Password = document.querySelector("#txtpassword");
+const Checkbox = document.querySelector("#show");
+
+Checkbox.addEventListener('click',function(){
+    const type =Password.getAttribute("type")===
+    "password" ? "text" : "password";
+    Password.setAttribute("type",type);
+});
